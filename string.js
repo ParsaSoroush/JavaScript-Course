3d5745b2b5ca9733923687833ded497323ba4c29
Parsa Soroush <parsakingkp@gmail.com>
2025-03-25 15:21:26 +0330

    String and adding strings

---
+++ string.js
@@ -0,0 +1,9 @@
+const firstName = "Parsa";
+const job = "developer";
+const birthYear = 2011;
+year = 2025
+
+const parsa = "I'm " + firstName + "and i'm a " + job + " and i'm " + (year - birthYear) + "years old.";
+
+
+console.log(parsa);
