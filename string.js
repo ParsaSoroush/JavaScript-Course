2378f6e53c5cc2c07807525920ea922c6856ed34
Parsa Soroush <parsakingkp@gmail.com>
2025-03-25 15:23:04 +0330

    Making the code better ans cleaner

--- string.js
+++ string.js
@@ -6,4 +6,7 @@ year = 2025
 const parsa = "I'm " + firstName + "and i'm a " + job + " and i'm " + (year - birthYear) + "years old.";
 
 
+const parsaBetter = `I'm ${firstName}, a ${job} and ${year - birthYear} years old.`;
+
 console.log(parsa);
+console.log(parsaBetter);
