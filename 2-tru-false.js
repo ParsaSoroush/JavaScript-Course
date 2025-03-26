3cf5945e95d2ed7fa5168c1d4768216b85be2845
Parsa Soroush <parsakingkp@gmail.com>
2025-03-26 12:37:29 +0330

    True Or False

---
+++ 2-tru-false.js
@@ -0,0 +1,15 @@
+let height = 10;
+
+if (height) {
+    console.log("Height is defined");
+} else {
+    console.log("Height is not defined");
+}
+
+let width = 0;
+
+if (width) {
+    console.log("Width is defined");
+} else {
+    console.log("Width is not defined");
+}
