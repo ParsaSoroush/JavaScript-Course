eda041b384761b8c17c46e799578d9353a9a29d8
Parsa Soroush <parsakingkp@gmail.com>
2025-03-26 18:53:44 +0330

    Logical Operators

---
+++ 3-logical-operators.js
@@ -0,0 +1,14 @@
+const hasDriversLicense = true;
+const hasGoodVision = false;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive){
+    console.log("Sarah is able to drive!");
+} else {
+    console.log("Someone else should drive...");
+}
