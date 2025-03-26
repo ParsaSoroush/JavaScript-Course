c0ae585d15ec43b46b8ca40c3e0a228dffa752a4
Parsa Soroush <parsakingkp@gmail.com>
2025-03-26 19:05:09 +0330

    switch Statment with week day example

---
+++ 4-switch-statment.js
@@ -0,0 +1,27 @@
+const day = 'friday';
+
+switch (day) {
+    case 'monday':
+        console.log("Plan course structure");
+        break;
+    case 'tuesday':
+        console.log("Prepare theory videos");
+        break;
+    case 'wednesday':
+        console.log("Write code examples");
+        break;
+    case 'thursday':
+        console.log("Enjoy the weekend");
+        break;
+    case 'friday':
+        console.log("Enjoy the weekend");
+        break;
+    case 'saturday':
+        console.log("Record final videos");
+        break;
+    case 'sunday':
+        console.log("Record videos");
+        break;
+    default:
+        console.log("Not a valid day");
+}
