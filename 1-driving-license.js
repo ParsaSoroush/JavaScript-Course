b8af051fad44dba2b2c50de0cbfe0f56cdc91906
Parsa Soroush <parsakingkp@gmail.com>
2025-03-26 12:29:15 +0330

    Driving License project

---
+++ 1-driving-license.js
@@ -0,0 +1,8 @@
+const age = 18;
+
+if (age >= 18) {
+    console.log("Sarah can start driving license 🚗");
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years.`)
+}
