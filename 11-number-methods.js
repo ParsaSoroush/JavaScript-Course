fce2e6112c493ff97989bf3152eb4db9ff44466a
Parsa Soroush <parsakingkp@gmail.com>
2025-05-10 10:58:28 +0330

    The number Methods

---
+++ 11-number-methods.js
@@ -0,0 +1,26 @@
+// toString()
+let a = 14;
+a.toString();
+
+// toExponential()
+let b = 14.234;
+b.toExponential(2);
+
+// toFixed()
+let c = 14.234;
+c.toFixed(2);
+
+// toPrecision()
+let d = 14.234;
+d.toPrecision(2);
+
+// valueOf()
+let e = 14.234;
+e.valueOf();
+
+console.log(a.toString());
+console.log(b.toExponential(2));
+console.log(c.toFixed(2));
+console.log(d.toPrecision(2));
+console.log(e.valueOf());
+
