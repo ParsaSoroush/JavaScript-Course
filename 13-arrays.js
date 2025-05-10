dc323f267ce76adfbf2963bea58dfd3ed33ba648
Parsa Soroush <parsakingkp@gmail.com>
2025-05-10 13:13:53 +0330

    JavaScript Arrays

---
+++ 13-arrays.js
@@ -0,0 +1,25 @@
+// # Create an Array
+const cars = ["Supra", "Buggati", "BMW"];
+
+// # Accessing an Array
+const cars2 = ["Mercedes Benz", "Toyota", "BMW"];
+let car = cars2[0];
+
+// # Changing an Array
+const cars3 = ["BMW", "Buggati", "Mercedes Benz"];
+cars3[1] = "Lamborghini";
+
+// # Length property in Arrays
+const langs = ["English", "French", "Spanish", "Turkish"];
+let length = langs.length;
+
+// # Adding Array Elements
+const fruits = ["Banana", "Orange", "Apple"];  
+fruits.push("Lemon");
+
+
+console.log(cars);
+console.log(car);
+console.log(cars3);
+console.log(length);
+console.log(fruits);
