37fdc08615d816788fc191ec69d31dc3fd8f4b32
Parsa Soroush <parsakingkp@gmail.com>
2025-05-11 20:57:50 +0330

    JavaScript Date Objects

---
+++ 15-date-objects.js
@@ -0,0 +1,17 @@
+// new Date()
+const newDate = new Date();
+
+// new Date(date string)
+const dateString = new Date("2025-01-01");
+
+// new Date(year, month, day, hour, min, secend, milisecend)
+const yearMonth = new Date(2025, 11, 11, 21, 33, 12, 36);
+
+// new Date(milisecend)
+const miliSecend = new Date(340000000000);
+
+
+console.log(newDate);
+console.log(dateString);
+console.log(yearMonth);
+console.log(miliSecend);
