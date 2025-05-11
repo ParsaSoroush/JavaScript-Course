16404aca28615a66bc5c764dd79e7dae44f06f70
Parsa Soroush <parsakingkp@gmail.com>
2025-05-11 16:05:33 +0330

    JavaScript Sroting an Array

---
+++ 14-sorting-array.js
@@ -0,0 +1,18 @@
+// Sroting an Array
+const cars = ["Lamborghini", "Ferrari", "Bugatti", "Supra"];
+cars.sort();
+
+
+// Reversing an Array
+const brands = ["LV", "Rolex", "Dior", "Tom Ford"];
+brands.sort();
+brands.reverse();
+
+// Sorting Numbers
+const numbers = [100, 50, 200, 150];
+numbers.sort((a, b) => a - b);
+
+
+console.log(cars);
+console.log(brands);
+console.log(numbers);
