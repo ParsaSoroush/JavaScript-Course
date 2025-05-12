a9f6b57b562084908b48ed1b918c272891a254d0
Parsa Soroush <parsakingkp@gmail.com>
2025-05-12 17:06:20 +0330

    JavaScript Math

---
+++ 17-math.js
@@ -0,0 +1,22 @@
+// Math.ciel()
+console.log(Math.ceil(4.6));
+
+// Math.floor()
+console.log(Math.floor(4.6));
+
+// Math.trunc()
+console.log(Math.trunc(4.6));
+
+// Math.sign()
+console.log(Math.sign(4));
+console.log(Math.sign(-4));
+console.log(Math.sign(0));
+
+// Math.pow()
+console.log(Math.pow(2, 3));
+
+// Math.sqrt()
+console.log(Math.sqrt(16));
+
+// Math.sqrt()
+console.log(Math.sqrt(16));
