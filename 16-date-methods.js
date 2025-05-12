7413024fdc64794a054ec8a5592c61bf8070eac5
Parsa Soroush <parsakingkp@gmail.com>
2025-05-12 16:16:41 +0330

    JavaScript Date Methods

---
+++ 16-date-methods.js
@@ -0,0 +1,21 @@
+// getFullYear()
+const date = new Date();
+console.log(date.getFullYear());
+
+// getMonth()
+console.log(date.getMonth());
+
+// getDate()
+console.log(date.getDate());
+
+// getHours()
+console.log(date.getHours());
+
+// getMinutes()
+console.log(date.getMinutes());
+
+// getSeconds()
+console.log(date.getSeconds());
+
+// getMilliseconds()
+console.log(date.getMilliseconds());
